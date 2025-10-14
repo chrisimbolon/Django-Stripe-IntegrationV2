96b5f0ab399230c2d2487f2a61a9e6fcea962f22
Christyan Simbolon <sambungdata@gmail.com>
2025-10-14 14:38:23 +0700

    home route start working

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,10 @@
+# payments/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     # path('session/<int:session_id>/', views.session_detail, name='session_detail'),
     # path('create-booking-intent/', views.create_booking_intent, name='create_booking_intent'),
     # path('booking/success/<int:booking_id>/', views.booking_success, name='booking_success'),
